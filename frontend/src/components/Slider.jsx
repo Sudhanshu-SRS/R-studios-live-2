bfff1bb88a952d11ac215b07545f918f49a70352
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2024-12-19 00:29:02 +0530

    height thodi kam kiya image ki

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets'
 const Slider = () => {
     return (
-        <div className='flex flex-col sm:flex-row  mb-8 '>
+        <div className='h-[800px] flex flex-col sm:flex-row  mb-8 '>
                 <img src={assets.Slider} className="w-full h-full transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0 " alt="" />
         </div>
     )
