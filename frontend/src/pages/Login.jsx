6e65f33e5de542537159ce07a71d89f5b1695575
Sudhanshu-SRS <sudhanshusakhare808@gmail.com>
2025-01-19 00:25:05 +0530

    added asstes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from "../context/ShopContext";
 import axios from "axios";
 import { toast } from "react-toastify";
 import { auth, provider, signInWithPopup } from "../config/firebase";
+import { assets } from "../mern-assets/assets";
 
 const Login = () => {
   const [currentState, setCurrentState] = useState("Login");
@@ -75,65 +76,82 @@ const Login = () => {
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
       </div>
-      {currentState === "Login" ? (
-        ""
-      ) : (
+      {currentState === "Sign Up" && (
+        <div className="flex items-center w-full px-3 py-2 border border-gray-800">
+          <img
+            src={assets.person_icon}
+            alt="person icon"
+            className="w-5 h-5 mr-2 "
+          />
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            className="flex-1 outline-none"
+            placeholder="Name"
+            required
+          />
+        </div>
+      )}
+      <div className="flex items-center w-full px-3 py-2 border border-gray-800">
+        <img src={assets.mail_icon} alt="email icon" className="w-5 h-5 mr-2" />
         <input
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          type="text"
-          className="w-full px-3 py-2 border border-gray-800"
-          placeholder="Name"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="email"
+          className="flex-1 outline-none"
+          placeholder="Email"
           required
         />
-      )}
-      <input
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-        type="email"
-        className="w-full px-3 py-2 border border-gray-800"
-        placeholder="Email"
-        required
-      />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-        type="password"
-        className="w-full px-3 py-2 border border-gray-800"
-        placeholder="Password"
-        required
-      />
+      </div>
+      <div className="flex items-center w-full px-3 py-2 border border-gray-800">
+        <img src={assets.lock_icon} alt="lock icon" className="w-5 h-5 mr-2" />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          className="flex-1 outline-none"
+          placeholder="Password"
+          required
+        />
+      </div>
       <button
         type="button"
         onClick={handleGoogleSignIn}
-        className=" px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 font-light px-8 py-2 mt-4"
+        className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150 font-light px-8 py-2 mt-4"
       >
         <img
-          class="w-6 h-6"
+          className="w-6 h-6"
           src="https://www.svgrepo.com/show/475656/google-color.svg"
           loading="lazy"
           alt="google logo"
-        ></img>
+        />
         Sign in with Google
       </button>
       <div className="w-full flex justify-between text-sm mt-[-8px]">
-        <p className=" cursor-pointer">Forgot your password?</p>
-        {currentState === "Login" ? (
-          <p
-            onClick={() => setCurrentState("Sign Up")}
-            className=" cursor-pointer"
-          >
-            Create account
-          </p>
-        ) : (
-          <p
-            onClick={() => setCurrentState("Login")}
-            className=" cursor-pointer"
-          >
-            Login Here
-          </p>
-        )}
-      </div>
+  {currentState === "Login" && (
+    <p className="cursor-pointer">Forgot your password?</p>
+  )}
+  {currentState === "Login" ? (
+    <p
+      onClick={() => setCurrentState("Sign Up")}
+      className="cursor-pointer"
+    >
+      Create account
+    </p>
+  ) : (
+    <p className="text-center w-full"
+      
+    >
+      All Ready Have An Accoutn?{''} 
+      <span
+      onClick={() => setCurrentState("Login")}
+     
+       className="cursor-pointer underline"> Login Here</span> 
+    </p>
+  )}
+</div>
+
       <button className="bg-black text-white font-light px-8 py-2 mt-4">
         {currentState === "Login" ? "Sign In" : "Sign Up"}
       </button>
